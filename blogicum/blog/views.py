cbeb48a86c2c89558edce14c7e30e15c5f5cc7fe
icier99 <icier99@mail.ru>
2025-06-04 10:34:38 +0300

    6

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -248,8 +248,6 @@ class PostDeleteView(LoginRequiredMixin, DeleteView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["form"] = forms.Form()
-        context["form"].instance = self.object
         return context
 
     def get_success_url(self):
